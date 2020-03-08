4271792ae9e7ec2a983d240e85e1bec81b85f827
Fernando Durier <fernando.durier@ibm.com>
2020-03-08 00:05:08 -0300

    Updates

---
+++ precache-manifest.41f8b65b4a6ebbd1878f80b1defd35c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1215c3e87a474194fba8",
+    "url": "/me/static/css/main.3d473195.chunk.css"
+  },
+  {
+    "revision": "1215c3e87a474194fba8",
+    "url": "/me/static/js/main.1215c3e8.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ae03d77dbe13327b514b9c3ade7b5472",
+    "url": "/me/index.html"
+  }
+];
