0cc6c319f30009c2510d61509dc9191af70c7cd6
FernandoDurier <fernando.durier@hotmail.com>
2021-08-25 14:18:44 -0300

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## Portfolio
-This is a portfolio of mine. Inspired and based on the deployed app : https://dbarochiya.github.io/me/
-
-This portfolio page is available at: https://fernandodurier.github.io/portfolio
---
+++ precache-manifest.2c90e9c879e0b05c7eb2bf95ec7fea08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6761ff96170631b92527",
+    "url": "/portfolio/static/css/main.3d473195.chunk.css"
+  },
+  {
+    "revision": "6761ff96170631b92527",
+    "url": "/portfolio/static/js/main.6761ff96.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "cdf8d534322216e427a90716598d0f72",
+    "url": "/portfolio/index.html"
+  }
+];
